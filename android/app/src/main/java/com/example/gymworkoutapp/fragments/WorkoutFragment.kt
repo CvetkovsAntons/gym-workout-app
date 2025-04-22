36039ff4b13ea7247fa95efff2e899e8e881ccd8
Antons Cvetkovs <antonscvetkovs12@gmail.com>
2025-04-22 07:17:40 +0300

    5 added simple workout fragment layout

--- android/app/src/main/java/com/example/gymworkoutapp/fragments/WorkoutFragment.kt
+++ android/app/src/main/java/com/example/gymworkoutapp/fragments/WorkoutFragment.kt
@@ -11,53 +11,16 @@ import com.example.gymworkoutapp.data.repository.UserRepository
 
 class WorkoutFragment(userRepository: UserRepository) : Fragment() {
 
-    private lateinit var recyclerView: RecyclerView
-//    private lateinit var workoutArrayList : ArrayList<>
-    private var idList = arrayListOf<String>()
-    private var workoutId : String? = ""
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_workouts, container, false)
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        recyclerView = (view.findViewById(R.id.premade_workouts)) as RecyclerView
-//        recyclerView.layoutManager = LinearLayoutManager(context)
-//        workoutArrayList = arrayListOf<PremadeWorkouts>()
-//
-//        recyclerView.visibility = View.GONE
-//
-//        refDb.addValueEventListener(object: ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//                workoutArrayList.clear()
-//                if (snapshot.exists()) {
-//                    for (workoutSnap in snapshot.children) {
-//                        val workoutData = workoutSnap.getValue(PremadeWorkouts::class.java)
-//                        workoutId = workoutSnap.key
-//                        workoutArrayList.add(workoutData!!)
-//                        idList.add(workoutId!!)
-//                    }
-//                    val adapter = PremadeWorkoutAdapter(workoutArrayList)
-//                    recyclerView.adapter = adapter
-//                    adapter.setOnItemClickListener(object: PremadeWorkoutAdapter.onItemClickListener{
-//                        override fun onItemClick(position: Int) {
-//                            val intent = Intent(activity, WorkoutPreviewActivity::class.java)
-//                            intent.putExtra("id", idList[position])
-//                            intent.putExtra("key", "premadeWorkouts")
-//                            startActivity(intent)
-//                        }
-//                    })
-//                }
-//                recyclerView.visibility = View.VISIBLE
-//            }
-//
-//            override fun onCancelled(error: DatabaseError) {
-//
-//            }
-//        })
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
 }
